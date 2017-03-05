c729f2e8b557cf3ccc0b830400d687c98911e7a8
Florent Viogne <viognef@gmail.com>
2017-03-05 18:36:23 +0100

    Reviewed code of Dialog
    Add javadoc
    Add setters and getters
    Add some this.
    Fix some bugs

--- src/Dialog.java
+++ src/Dialog.java
@@ -1,10 +1,27 @@
 import java.util.Arrays;
 
+/**
+ * This class represents a dialog
+ * @author Florent
+ *
+ */
 public class Dialog {
 	
-	public String text;
-	public Choice[] choices;
+	/**
+	 * Text to make the choice
+	 */
+	private String text;
+
+	/**
+	 * Possible choices 
+	 */
+	private Choice[] choices;
 	
+	/**
+	 * Dialog's constructor with given text and given choices 
+	 * @param text , text to make the choice
+	 * @param choices , possible choices
+	 */
 	public Dialog(String text, Choice choices[]){
 		this.text = text;
 		this.choices = choices;
@@ -12,15 +29,40 @@ public class Dialog {
 
 	@Override
 	public String toString() {
-		String message = "Dialog [text=" + text + ", choices=";
+		String message = "Dialog [text=" + this.text + ", choices=";
 		for(int i=0;i<4;i++){
-			message = message + choices[i].text + " ";
+			message = message + this.choices[i].getText() + " ";
 		}
 		message = message + "]";
 		return message;
 	}
 
-	
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return this.text;
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+	/**
+	 * @return the choices
+	 */
+	public Choice[] getChoices() {
+		return this.choices;
+	}
+
+	/**
+	 * @param choices the choices to set
+	 */
+	public void setChoices(Choice[] choices) {
+		this.choices = choices;
+	}
 	
 	
 	
