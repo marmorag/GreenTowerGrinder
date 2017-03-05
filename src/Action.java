308e848a831799300c81b487f4e629dd098149f0
Florent Viogne <viognef@gmail.com>
2017-03-05 18:28:46 +0100

    Minimal Action Class
    Writed javadoc
    Implementation of default constructor for tests

--- src/Action.java
+++ src/Action.java
@@ -1,4 +1,15 @@
-
-public class Action {
-
+/**
+ * This class represents an action in the game.
+ * @author Florent
+ *
+ */
+public class Action 
+{
+	/**
+	 * Default Action's constructor
+	 */
+	public Action()
+	{
+		
+	}
 }
