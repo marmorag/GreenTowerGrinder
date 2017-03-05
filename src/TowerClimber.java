46f2cee989697e61b67dce40e88f59bc4c73245a
Florent Viogne <viognef@gmail.com>
2017-03-05 18:34:49 +0100

    Reviewed code TowerClimber class
    Add javadoc
    Add actions' instantation
    Add action parameters in Choice's constructor

--- src/TowerClimber.java
+++ src/TowerClimber.java
@@ -1,12 +1,26 @@
-
+/**
+ * Main class of the game. 
+ * Entry point of the application.
+ * @author Florent
+ *
+ */
 public class TowerClimber {
 
+	/**
+	 * Entry point
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
-		Choice A = new Choice("1");
-		Choice B = new Choice("2");
-		Choice C = new Choice("3");
-		Choice D = new Choice("4");
+		Action action1 = new Action();
+		Action action2 = new Action();
+		Action action3 = new Action();
+		Action action4 = new Action();
+		
+		Choice A = new Choice("1", action1);
+		Choice B = new Choice("2", action2);
+		Choice C = new Choice("3", action3);
+		Choice D = new Choice("4", action4);
 		
 		Choice[] choices = Choice.getTab(A, B, C, D);
 		
