8158a6f32a198cd6df62061a04d8c1f469505a1e
Rémi <remi.gavin38@gmail.com>
2017-03-31 14:48:33 +0200

    TicTacToe finished
    User example in MainTest class

--- src/ticTacToe/MainTest.java
+++ src/ticTacToe/MainTest.java
@@ -5,12 +5,16 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TicTacToe ttt = new TicTacToe();
+		System.out.println(ttt.toString());
 		while(true){
 			if(ttt.PlayerTurn())
 				break;
+			System.out.println(ttt.toString());
 			if(ttt.GameTurn())
 				break;
+			System.out.println(ttt.toString());
 		}
+		System.out.println(ttt.toString());
 		if(ttt.getWinner() == 1){
 			System.out.println("Bravo");
 		}else{
--- src/ticTacToe/TicTacToe.java
+++ src/ticTacToe/TicTacToe.java
@@ -1,5 +1,6 @@
 package ticTacToe;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -10,13 +11,27 @@ public class TicTacToe {
 	private int[][] grid = new int [3][3];
 	private int winner;
 	
+	public TicTacToe(){
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				this.grid[i][j] = 0;
+			}
+		}
+	}
 	
 	public boolean PlayerTurn(){
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		int p1 = Integer.parseInt(sc.nextLine());
-		int p2 = Integer.parseInt(sc.nextLine());
-		
+		int p1=0,p2=0;
+		while(true){
+			p1 = Integer.parseInt(sc.nextLine());
+			p2 = Integer.parseInt(sc.nextLine());
+			if(this.grid[p1][p2] == 0){
+				this.grid[p1][p2] = 1;
+				break;
+			}
+			System.out.println("Cette case n'est pas vide");
+		}
 		if(this.grid[p1][0] == 1 && this.grid[p1][1] == 1 && this.grid[p1][2] == 1){
 			this.winner = PLAYER;
 			return true;
@@ -25,22 +40,29 @@ public class TicTacToe {
 			this.winner = PLAYER;
 			return true;
 		}
-		//Condition pour la diagonale
+		if(this.grid[0][0] == 1 && this.grid[1][1] == 1 && this.grid[2][2] ==1){
+			this.winner = PLAYER;
+			return true;
+		}
+		if(this.grid[2][0] == 1 && this.grid[1][1] == 1 && this.grid[0][2] == 1){
+			this.winner = GAME;
+			return true;
+		}
 		return false;
 	}
 	
 	public boolean GameTurn(){
-		boolean b = false;
 		int p1 = 0,p2 = 0;
-		while(b == false){
-			p1 = (int) Math.random() * 3;
-			p2 = (int) Math.random() * 3;
+		Random r = new Random();
+		while(true){
+			p1 = (int) r.nextInt(3);
+			p2 = (int) r.nextInt(3);
 			if(this.grid[p1][p2] == 0){
 				this.grid[p1][p2] = 2;
-				this.winner = GAME;
-				b = true;
+				break;
 			}
 		}
+		System.out.println("ok");
 		if(this.grid[p1][0] == 2 && this.grid[p1][1] == 2 && this.grid[p1][2] == 2){
 			this.winner = GAME;
 			return true;
@@ -49,12 +71,28 @@ public class TicTacToe {
 			this.winner = GAME;
 			return true;
 		}
-		//Condition pour la diagonale
+		if(this.grid[0][0] == 2 && this.grid[1][1] == 2 && this.grid[2][2] ==2){
+			this.winner = GAME;
+			return true;
+		}
+		if(this.grid[2][0] == 2 && this.grid[1][1] == 2 && this.grid[0][2] == 2){
+			this.winner = GAME;
+			return true;
+		}
 		return false;
 	}
 	
 	public int getWinner(){
 		return this.winner;
 	}
+
+	@Override
+	public String toString() {
+		return this.grid[0][0]+"|"+this.grid[0][1]+"|"+this.grid[0][2]+"|\n"+
+				this.grid[1][0]+"|"+this.grid[1][1]+"|"+this.grid[1][2]+"|\n"+
+				this.grid[2][0]+"|"+this.grid[2][1]+"|"+this.grid[2][2]+"|\n";
+	}
+	
+	
 	
 }
