552ab27bd7b6ed2e4910fd05acf52a4342d4cd77
Rémi <remi.gavin38@gmail.com>
2017-03-31 17:37:18 +0200

    Add shifumi javadoc

--- src/shifumi/Shifumi.java
+++ src/shifumi/Shifumi.java
@@ -1,13 +1,23 @@
 package shifumi;
 
+/*
+ * 
+ * @authors Gavin Rémi & Barama Rayan
+ */
 public class Shifumi {
 	
+	/*
+	 * Return if player win or not
+	 * @param {String} Player's sign
+	 * @return true if player win and false if not
+	 */
 	public static boolean play(String sign){
 		
-		//Transformation string -> Sign
+		//transform string -> Sign
 		Sign playersign = Sign.valueOf(sign);
 		Sign random = Sign.getRandom();
 		
+		//Test all possibilities of shifumi
 		if(playersign == Sign.PAPER){
 			if(random == Sign.ROCK){
 				return true;
--- src/shifumi/Sign.java
+++ src/shifumi/Sign.java
@@ -1,10 +1,17 @@
 package shifumi;
 
+/*
+ * All shifumi sign
+ */
 public enum Sign {
 	ROCK,
 	PAPER,
 	SCISSORS;
-
+	
+	/*
+	 * Return random sign among enum
+	 * @return {Sign} random sign among enum
+	 */
 	public static Sign getRandom() {
 		return values()[(int) (Math.random() * values().length-1)];
 	}
