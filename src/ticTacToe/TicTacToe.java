f7eda24ec42d5528986b5c4145466a59dcae00e0
Rémi <remi.gavin38@gmail.com>
2017-03-31 17:25:45 +0200

    Add TicTacToe javadoc

--- src/ticTacToe/TicTacToe.java
+++ src/ticTacToe/TicTacToe.java
@@ -2,15 +2,29 @@ package ticTacToe;
 
 import java.util.Random;
 import java.util.Scanner;
-
+/*
+ * MiniGame TicTacToe
+ * @author Gavin Rémi
+ */
 public class TicTacToe {
-	
+	/*
+	 *  Constant for winner
+	 */
 	private final static int PLAYER = 1;
 	private final static int GAME = 0;
 	
+	/*
+	 * Grid of TicTacToe, contains cross and round
+	 */
 	private int[][] grid = new int [3][3];
+	/*
+	 * Winner of the game
+	 */
 	private int winner;
 	
+	/*
+	 * Fill the grid with 0
+	 */
 	public TicTacToe(){
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
@@ -19,16 +33,20 @@ public class TicTacToe {
 		}
 	}
 	
+	/*
+	 * Player turn : he types the 2 indexes one by one
+	 * @return true if he win and false if not
+	 */
 	public boolean PlayerTurn(){
-		if(this.isFull()){
+		if(this.isFull()){ // Test if no one have win
 			System.out.println("Egalite, veuillez rejouer");
 			this.resetGrid();
 		}
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int p1=0,p2=0;
-		while(true){
-			p1 = Integer.parseInt(sc.nextLine());
+		while(true){ //Get index by the user
+			p1 = Integer.parseInt(sc.nextLine()); 
 			p2 = Integer.parseInt(sc.nextLine());
 			if(this.grid[p1][p2] == 0){
 				this.grid[p1][p2] = 1;
@@ -36,6 +54,7 @@ public class TicTacToe {
 			}
 			System.out.println("Cette case n'est pas vide");
 		}
+		// Test if player made a column or a row or a diagonal
 		if(this.grid[p1][0] == 1 && this.grid[p1][1] == 1 && this.grid[p1][2] == 1){
 			this.winner = PLAYER;
 			return true;
@@ -49,20 +68,24 @@ public class TicTacToe {
 			return true;
 		}
 		if(this.grid[2][0] == 1 && this.grid[1][1] == 1 && this.grid[0][2] == 1){
-			this.winner = GAME;
+			this.winner = PLAYER;
 			return true;
 		}
 		return false;
 	}
 	
+	/*
+	 * Bot turn, random numbers are generate to place a sign
+	 * @return true if he win and false if not
+	 */
 	public boolean GameTurn(){
-		if(this.isFull()){
+		if(this.isFull()){ // Test if no one have win
 			System.out.println("Egalite, veuillez rejouer");
 			this.resetGrid();
 		}
 		int p1 = 0,p2 = 0;
 		Random r = new Random();
-		while(true){
+		while(true){ // Get random number between 0 and grid length
 			p1 = (int) r.nextInt(3);
 			p2 = (int) r.nextInt(3);
 			if(this.grid[p1][p2] == 0){
@@ -70,7 +93,7 @@ public class TicTacToe {
 				break;
 			}
 		}
-		System.out.println("ok");
+		// Test if player made a column or a row or a diagonal
 		if(this.grid[p1][0] == 2 && this.grid[p1][1] == 2 && this.grid[p1][2] == 2){
 			this.winner = GAME;
 			return true;
@@ -90,17 +113,29 @@ public class TicTacToe {
 		return false;
 	}
 	
+	/*
+	 * Return game winner 
+	 * @return winner of the game;
+	 */
 	public int getWinner(){
 		return this.winner;
 	}
-
+	
 	@Override
+	/*
+	 * Return Game grid fill in with players signs
+	 * @return Game grid
+	 */
 	public String toString() {
 		return this.grid[0][0]+"|"+this.grid[0][1]+"|"+this.grid[0][2]+"|\n"+
 				this.grid[1][0]+"|"+this.grid[1][1]+"|"+this.grid[1][2]+"|\n"+
 				this.grid[2][0]+"|"+this.grid[2][1]+"|"+this.grid[2][2]+"|\n";
 	}
 	
+	/*
+	 * Return if grid is full or not
+	 * @return true if grid is full
+	 */
 	public boolean isFull(){
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
@@ -112,6 +147,9 @@ public class TicTacToe {
 		return true;
 	}
 	
+	/*
+	 * Reset game grid (if have equality in precedent game)
+	 */
 	public void resetGrid(){
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
