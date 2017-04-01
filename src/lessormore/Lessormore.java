2d1da26f687fa8754a27b14c9228bd5c64befdbc
Rayan <rayan.brm@gmail.com>
2017-04-01 10:45:11 +0200

    add a new game ( less or more )

---
+++ src/lessormore/Lessormore.java
@@ -0,0 +1,49 @@
+package lessormore;
+
+public class Lessormore {
+
+	/*
+	 * This class represent a Lessormore game
+	 * @author Rayan Barama
+	 */
+	
+	
+	/*
+	 * the first card which is randomly picked
+	 */
+	public static int base = (int)(Math.random() * (10-1)) + 1;
+	/*
+	 * the new card which is randomly picked
+	 */
+	public static int randomcard = (int)(Math.random() * (10-1)) + 1;
+
+
+	/*
+	 * return if player win or not
+	 * @param {String} User decision
+	 * @return {boolean} true if the game is won and false if it is not
+	 */ 
+	public static boolean play(String userChoice){
+
+		if(userChoice.equals("MORE")){
+			if(randomcard<base){
+				return false;
+			}
+			else{
+				return true;
+			}
+
+		}
+		else if(userChoice.equals("LESS")){
+			if(randomcard<base){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	}
+}
---
+++ src/lessormore/Maintest.java
@@ -0,0 +1,26 @@
+package lessormore;
+
+import java.util.Scanner;
+
+public class Maintest {
+
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		System.out.println(Lessormore.base);
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		String sign = sc.nextLine();
+		
+
+		if(Lessormore.play(sign.toUpperCase()) == true){
+			System.out.println("BRAVO!");
+		}else{
+			System.out.println("Dommage :(");
+		}
+
+	}
+
+}
