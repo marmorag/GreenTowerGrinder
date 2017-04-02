97e89eba1f5a7d41d267998624adbb871cde87b6
Rémi <remi.gavin38@gmail.com>
2017-04-02 10:57:01 +0200

    Rewrite GamblingMachine

--- src/gamblingmachine/GamblingMachine.java
+++ src/gamblingmachine/GamblingMachine.java
@@ -1,6 +1,6 @@
 package gamblingmachine;
 
-public class GamblingMachine {
+/*public class GamblingMachine {
 
 	public static int[][] grid = new int [1][3];
 	public static String result;
@@ -17,6 +17,31 @@ public class GamblingMachine {
 		}
 		else{
 			result="FAIL :/";
+		}
+	}
+}*/
+
+/**
+ * @author Florent & Rémi
+ */
+public class GamblingMachine{
+	/**
+	 * Generate 3 random number between 0 and 2
+	 * The player win if all 3 numbers and equals
+	 * @return true if player wins false if not
+	 */
+	public static boolean play(){
+		int[] grid = new int[3];
+		grid[0] = (int)(Math.random() * (3-0)) + 0;
+		grid[1] = (int)(Math.random() * (3-0)) + 0;
+		grid[2] = (int)(Math.random() * (3-0)) + 0;
+		
+		if(grid[0] == grid[1] && grid[1] == grid[2]){
+			System.out.println(grid[0]+"|"+grid[1]+"|"+grid[2]);
+			return true;
+		}else{
+			System.out.println(grid[0]+"|"+grid[1]+"|"+grid[2]);
+			return false;
 		}
 	}
 }
--- src/gamblingmachine/Maintest.java
+++ src/gamblingmachine/Maintest.java
@@ -5,10 +5,15 @@ public class Maintest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-
+		/*
 		GamblingMachine.play();
 		System.out.println(GamblingMachine.grid[0][0]+"|"+GamblingMachine.grid[0][1]+"|"+GamblingMachine.grid[0][2]);
 		System.out.println(GamblingMachine.result);
+		*/
+		if(GamblingMachine.play())
+			System.out.println("JACKPOT");
+		else
+			System.out.println("FAIL");
 	}
 	
 }
