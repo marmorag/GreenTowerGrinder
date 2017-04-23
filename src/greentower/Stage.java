e2a6a6cad437cd85fb679855bee6b332f42c78c1
Florent Viogne <viognef@gmail.com>
2017-04-23 11:26:51 +0200

    [LOGIC] Made some changes
    - Created the class Stage 
    - Deleted TowerClimber (useless)
    - Deleted "// TODO Auto-generated method stub" in gamblingmachine.Maintest

---
+++ src/greentower/Stage.java
@@ -0,0 +1,15 @@
+package greentower;
+
+/**
+ * This interface enables to launch a stage (MiniGame or Choice, whatever)
+ * 
+ * @author Florent
+ *
+ */
+public interface Stage
+{
+	/**
+	 * Play the current stage (MiniGame or Choice, whatever)
+	 */
+	public void playStage();
+}
--- src/greentower/gamblingmachine/Maintest.java
+++ src/greentower/gamblingmachine/Maintest.java
@@ -3,7 +3,6 @@ package greentower.gamblingmachine;
 public class Maintest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 			
 		/*
 		GamblingMachine.play();
