1a22deae3d2c6387ee4cfca442d5e12be2403d24
marmorag <marmorag@PC-B102-02.iut-valence.fr>
2017-05-04 15:48:25 +0200

    [LOGIC]Modified Lessormore to match with main scheme

--- src/greentower/lessormore/Lessormore.java
+++ src/greentower/lessormore/Lessormore.java
@@ -1,43 +1,70 @@
 package greentower.lessormore;
 
-public class Lessormore {
+import java.util.Scanner;
 
-	/*
-	 * This class represent a Lessormore game
-	 * @author Rayan Barama
-	 */
-	
-	
-	/*
+import greentower.core.MiniGame;
+
+/**
+ * This class represent a Lessormore game
+ * @author Rayan Barama
+ */
+public class Lessormore extends MiniGame{
+	/**
 	 * the first card which is randomly picked
 	 */
-	public static int base = (int)(Math.random() * (10-1)) + 1;
-	/*
+	public static int base;
+	/**
 	 * the new card which is randomly picked
 	 */
-	public static int randomcard = (int)(Math.random() * (10-1)) + 1;
+	public static int randomcard;
 
+	public Lessormore(int id)
+	{
+		super(id);
+		this.base = (int)(Math.random() * (10-1)) + 1;
+		this.randomcard = (int)(Math.random() * (10-1)) + 1;
+
+	}
 
 	/*
 	 * return if player win or not
 	 * @param {String} User decision
 	 * @return {boolean} true if the game is won and false if not
-	 */ 
-	public static boolean play(String userChoice){
+	 */
+	public Stage playStage(){
+		boolean result;
+
+		System.out.println(Lessormore.base);
+		Scanner sc = new Scanner(System.in);
+		String userChoice = sc.nextLine();
+		userChoice = userChoice.toUpperCase();
+
 
 		if(userChoice.equals("MORE")){
 			if(randomcard<base)
-				return false;
+				result = false;
 			else
-				return true;
+				result = true;
 		}
 		else if(userChoice.equals("LESS")){
 			if(randomcard<base)
-				return true;
+				result = true;
 			else
-				return false;
+				result = false;
 		}
 		else
-			return false;
+			result = false;
+
+		if(result)
+		{
+			System.out.println("BRAVO!");
+			return this.nextStages[0];
+		}
+		else
+		{
+			System.out.println("Dommage :(");
+			return this.nextStages[1];
+		}
+
 	}
 }
