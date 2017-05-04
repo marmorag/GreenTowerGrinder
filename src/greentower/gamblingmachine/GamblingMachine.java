ed57e4fd142c0e6055b8038a9185c3e7937f13ad
marmorag <marmorag@PC-B102-02.iut-valence.fr>
2017-05-04 15:38:21 +0200

    [LOGIC]Modified GamblingMachine to match with main scheme

--- src/greentower/gamblingmachine/GamblingMachine.java
+++ src/greentower/gamblingmachine/GamblingMachine.java
@@ -1,5 +1,6 @@
 package greentower.gamblingmachine;
 import greentower.core.MiniGame;
+import greentower.core.Stage;
 
 /*public class GamblingMachine {
 
@@ -26,12 +27,19 @@ import greentower.core.MiniGame;
  * @author Florent & Rémi
  */
 public class GamblingMachine extends MiniGame{
+
+	public GamblingMachine(int id)
+	{
+		super(id);
+	}
+
 	/**
 	 * Generate 3 random number between 0 and 2
 	 * The player win if all 3 numbers and equals
 	 * @return true if player wins false if not
 	 */
-	public static boolean play(){
+	public Stage playStage(){
+		boolean result;
 		int[] grid = new int[3];
 		grid[0] = (int)(Math.random() * (3-0)) + 0;
 		grid[1] = (int)(Math.random() * (3-0)) + 0;
@@ -39,10 +47,21 @@ public class GamblingMachine extends MiniGame{
 
 		if(grid[0] == grid[1] && grid[1] == grid[2]){
 			System.out.println(grid[0]+"|"+grid[1]+"|"+grid[2]);
-			return true;
+			result = true;
 		}else{
 			System.out.println(grid[0]+"|"+grid[1]+"|"+grid[2]);
-			return false;
+			result = false;
+		}
+
+		if(result)
+		{
+			System.out.println("JACKPOT");
+			return this.nextStages[0];
+		}
+		else
+		{
+			System.out.println("FAIL");
+			return this.nextStages[1];
 		}
 	}
 }
