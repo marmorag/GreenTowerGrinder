4801d04e148fc5cfc6c62cd7287800b3d937b639
AxelleDmz <axelle.delomez@iut-valence.fr>
2017-05-30 12:36:13 +0200

    Answers added

--- src/greentower/core/Answers.java
+++ src/greentower/core/Answers.java
@@ -16,7 +16,7 @@ public enum Answers {
 	/**
 	 * Stage 2's ANSWERS
 	 */
-	ANSWERS_STAGE2(new String[] {""}),
+	ANSWERS_STAGE2(new String[] {"Oui","Non"}),
 
 	/**
 	 * Stage 3's ANSWERS
@@ -26,77 +26,77 @@ public enum Answers {
 	/**
 	 * Stage 4's ANSWERS
 	 */
-	ANSWERS_STAGE4(new String[] {""}),
+	ANSWERS_STAGE4(new String[] {""}), // VICTOIRE OU DEFAITE
 
 	/**
 	 * Stage 5's ANSWERS
 	 */
-	ANSWERS_STAGE5(new String[] {""}),
+	ANSWERS_STAGE5(new String[] {""}), // VICTOIRE OU DEFAITE
 
 	/**
 	 * Stage 6's ANSWERS
 	 */
-	ANSWERS_STAGE6(new String[] {""}),
+	ANSWERS_STAGE6(new String[] {"1","16","30","46"}),
 
 	/**
 	 * Stage 7's ANSWERS
 	 */
-	ANSWERS_STAGE7(new String[] {""}),
+	ANSWERS_STAGE7(new String[] {""}), // VICTOIRE OU DEFAITE
 
 	/**
 	 * Stage 8's ANSWERS
 	 */
-	ANSWERS_STAGE8(new String[] {""}),
+	ANSWERS_STAGE8(new String[] {"D'accord.."}),
 
 	/**
 	 * Stage 9's ANSWERS
 	 */
-	ANSWERS_STAGE9(new String[] {""}),
+	ANSWERS_STAGE9(new String[] {"J'accepte","Je refuse"}),
 
 	/**
 	 * Stage 10's ANSWERS
 	 */
-	ANSWERS_STAGE10(new String[] {""}),
+	ANSWERS_STAGE10(new String[] {"Continuer"}),
 
 	/**
 	 * Stage 11's ANSWERS
 	 */
-	ANSWERS_STAGE11(new String[] {""}),
+	ANSWERS_STAGE11(new String[] {"En roulant tous à vitesse constante","En roulant tous à droite"}),
 
 	/**
 	 * Stage 12's ANSWERS
 	 */
-	ANSWERS_STAGE12(new String[] {""}),
+	ANSWERS_STAGE12(new String[] {"Continuer"}),
 
 	/**
 	 * Stage 13's ANSWERS
 	 */
-	ANSWERS_STAGE13(new String[] {""}),
+	ANSWERS_STAGE13(new String[] {""}), // VICTOIRE OU DEFAITE
 
 	/**
 	 * Stage 14's ANSWERS
 	 */
-	ANSWERS_STAGE14(new String[] {""}),
+	ANSWERS_STAGE14(new String[] {""}), // VICTOIRE OU DEFAITE
 
 	/**
 	 * Stage 15's ANSWERS
 	 */
-	ANSWERS_STAGE15(new String[] {""}),
+	ANSWERS_STAGE15(new String[] {"Réveiller le gardien","Faire une sieste à ses côtés"}),
 
 	/**
 	 * Stage 16's ANSWERS
 	 */
-	ANSWERS_STAGE16(new String[] {""}),
+	ANSWERS_STAGE16(new String[] {"Un bébé","Une ombre"}),
 
 	/**
 	 * Stage 17's ANSWERS
 	 */
-	ANSWERS_STAGE17(new String[] {""}),
+	ANSWERS_STAGE17(new String[] {"Continuer"}),
 
 	/**
 	 * Stage 18's ANSWERS
 	 */
-	ANSWERS_STAGE18(new String[] {""}),
+	ANSWERS_STAGE18(new String[] {"Merci !"}),
 
 	/**
 	 * Stage 19's ANSWERS
@@ -106,22 +106,22 @@ public enum Answers {
 	/**
 	 * Stage 20's ANSWERS
 	 */
-	ANSWERS_STAGE20(new String[] {""}),
+	ANSWERS_STAGE20(new String[] {"Continuer"}),
 
 	/**
 	 * Stage 21's ANSWERS
 	 */
-	ANSWERS_STAGE21(new String[] {""}),
+	ANSWERS_STAGE21(new String[] {"Continuer"}),
 
 	/**
 	 * Stage 22's ANSWERS
 	 */
-	ANSWERS_STAGE22(new String[] {""}),
+	ANSWERS_STAGE22(new String[] {"Continuer"}),
 
 	/**
 	 * Stage 23's ANSWERS
 	 */
-	ANSWERS_STAGE23(new String[] {""});
+	ANSWERS_STAGE23(new String[] {""}); // VICTOIRE OU DEFAITE
 
 
 	/**
