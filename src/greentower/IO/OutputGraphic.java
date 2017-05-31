aebb9e5cb5ac36ae1e7d53d57517ea8dc0713287
marmorag <g.marmo@hotmail.fr>
2017-06-01 01:00:16 +0200

    [LOGIC] Modified Main and Game.play

--- src/greentower/IO/OutputGraphic.java
+++ src/greentower/IO/OutputGraphic.java
@@ -31,45 +31,52 @@ public class OutputGraphic extends JFrame implements Output{
 		
 	}
 
+	
+	
+	
+	
+	/* ******************************* THE FOLLOWING METHODS SHOULDN'T BE IMPLEMENTED ******************************* */
+	
+	
 	@Override
 	public void notifyWin(int nbCoups, String motATrouver) {
-		// TODO Auto-generated method stub
+		// VOID FOR NON USED METHOD AT THIS STAGE
 		
 	}
 
 	@Override
 	public void erreurLettre() {
-		// TODO Auto-generated method stub
+		// VOID FOR NON USED METHOD AT THIS STAGE
 		
 	}
 
 	@Override
 	public void notifyLoose(String motATrouver) {
-		// TODO Auto-generated method stub
+		// VOID FOR NON USED METHOD AT THIS STAGE
 		
 	}
 
 	@Override
 	public void demanderCaractere() {
-		// TODO Auto-generated method stub
+		// VOID FOR NON USED METHOD AT THIS STAGE
 		
 	}
 
 	@Override
 	public void afficherPendu(int nbErreur, String motAAfficher) {
-		// TODO Auto-generated method stub
+		// VOID FOR NON USED METHOD AT THIS STAGE
 		
 	}
 
 	@Override
 	public void showGame() {
-		// TODO Auto-generated method stub
+		// VOID FOR NON USED METHOD AT THIS STAGE
 		
 	}
 
 	@Override
 	public void showAnswers(String[] answers) {
-		// TODO Auto-generated method stub
+		// VOID FOR NON USED METHOD AT THIS STAGE
 		
 	}
 
--- src/greentower/core/Game.java
+++ src/greentower/core/Game.java
@@ -1,8 +1,10 @@
 package greentower.core;
 
-import java.io.IOException;
-
 import greentower.IO.Output;
+import greentower.IO.OutputConsole;
+import greentower.IO.OutputGraphicChoice;
+import greentower.minigames.lessormore.OutputGraphicLessOrMore;
+import greentower.minigames.tictactoe.OutputGraphicTicTacToe;
 
 /**
  * This class represents a game of The Green Tower Grinder
@@ -34,15 +36,22 @@ public class Game
 	private Output display;
 
 	/**
+	 * True if the game is executed in graphical mode, false else
+	 */
+	private boolean isGraphical;
+
+	/**
 	 * Default constructor
 	 * @param player who launched the game
 	 * @param display
+	 * @param isGraphical 
 	 */
-	public Game(Player player, Output display)
+	public Game(Player player, Output display, boolean isGraphical)
 	{
+		this.isGraphical = isGraphical;
 		this.numberOfRounds = 0;
 		this.player = player;
-		this.currentStage = ListOfStages.getStageAt(0);
+		this.currentStage = ListOfStages.getStageAt(1);
 		this.display = display;
 	}
 
@@ -51,26 +60,75 @@ public class Game
 	 */
 	public void play()
 	{
-		while(this.currentStage.getFinalStage() == false)
+		try
 		{
-			try 
+			int indexOfCurrentStages = 1;
+			while(this.currentStage.getFinalStage() == false)
 			{
+				
+				if(this.isGraphical)
+					this.display = updateDisplayMode(indexOfCurrentStages);
+				else
+					this.display = updateDisplayMode(-1);
+				
 				int result = this.currentStage.playStage(this.display);
-				this.currentStage = Tower.getNextStage(Tower.getStageIndex(this.currentStage), result);
-				//this.currentStage = Tower.getNextStage(Tower.getStageIndex(this.currentStage), this.currentStage.playStage(this.display));
-				this.numberOfRounds++;
-			} catch (IOException e) {
-				System.out.println("Problème avec l'entrée");
-				System.out.println("Veuillez recommencer");
-			} catch (Exception e)
-			{
-				e.printStackTrace();
-				System.out.println("Un problème est survenu, veuillez recommencer");
-				System.out.println("Toutes nos excuses");
+				this.currentStage = ListOfStages.getNextStages(ListOfStages.getStageIndex(this.currentStage), result);
 			}
+			
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+//		while(this.currentStage.getFinalStage() == false)
+//		{
+//			try 
+//			{
+//				int result = this.currentStage.playStage(this.display);
+//				this.currentStage = Tower.getNextStage(Tower.getStageIndex(this.currentStage), result);
+//				//this.currentStage = Tower.getNextStage(Tower.getStageIndex(this.currentStage), this.currentStage.playStage(this.display));
+//				this.numberOfRounds++;
+//			} catch (IOException e) {
+//				System.out.println("Problème avec l'entrée");
+//				System.out.println("Veuillez recommencer");
+//			} catch (Exception e)
+//			{
+//				e.printStackTrace();
+//				System.out.println("Un problème est survenu, veuillez recommencer");
+//				System.out.println("Toutes nos excuses");
+//			}
+//		}
+	}
+
+	
+	private static Output updateDisplayMode(int indexOfCurrentStages)
+	{
+		switch (indexOfCurrentStages)
+		{
+		case -1:
+			return new OutputConsole();
+		case 3 :
+			return new OutputGraphicLessOrMore();
+		case 4:
+			return new OutputGraphicTicTacToe();
+		case 7:
+			return new OutputGraphicLessOrMore();
+			// TODO add implementation of RushHour
+		case 13:
+			return new OutputGraphicLessOrMore();
+			// TODO add implementation of Othello
+		case 22:
+			return new OutputGraphicLessOrMore();
+			// TODO add implementation of MasterMind
+		default:
+			return new OutputGraphicChoice();
 		}
 	}
 
+	
 	/**
 	 * Enables player to leave the current game
 	 */
