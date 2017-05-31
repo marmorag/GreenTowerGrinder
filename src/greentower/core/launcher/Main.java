d2f79536bd9a895d27f6b1729442eaca68bf139d
marmorag <g.marmo@hotmail.fr>
2017-06-01 01:04:01 +0200

    [EVENT] Game v1.0

--- src/greentower/core/launcher/Main.java
+++ src/greentower/core/launcher/Main.java
@@ -18,6 +18,7 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
+		// Game v1.0
 		System.out.println("****** BIENVENUE DANS THE GREEN TOWER GRINDER ******");
 		new Game(new Player("Coucou"), new OutputConsole(), false).play();
 		
