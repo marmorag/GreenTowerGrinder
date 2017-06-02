771860a45b5c1c62ef7e73796718686ce287d1ab
Axelle <Axelle@DESKTOP-GQP648J.home>
2017-06-02 19:55:09 +0200

    [FIX] TestMasterMind

--- src/test/TestMasterMind.java
+++ src/test/TestMasterMind.java
@@ -35,7 +35,44 @@ public class TestMasterMind {
 		
 		assertEquals("ERREUR : A,A,A,A ne devrait pas être une combination valide ",res,MasterMind.stringChecker(code));
 		
-		//test n°4
+		//test n°4 - la longueur de la combinaison est invalide
+		code = "RRRRR";
+		res = false;
+		
+		assertEquals("ERREUR : CHAINE TROP LONGUE R,R,R,R,R ne devrait pas être une combination valide ",res,MasterMind.stringChecker(code));
+		
+		//test n°5 - la longueur de la combinaison est invalide
+		code = "RRR";
+		res = false;
+		
+		assertEquals("ERREUR : CHAINE TROP COURTE R,R,R ne devrait pas être une combination valide ",res,MasterMind.stringChecker(code));
+		
+		//test n°6 - la combinaison contient des espaces
+		code = "R RR";
+		res = false;
+		
+		assertEquals("ERREUR : R, ,R,R ne devrait pas être une combination valide ",res,MasterMind.stringChecker(code));
+		
+		// !--CAS LIMITES--!
+		
+		//test n°7 - la combinaison est vide
+		code = (" ");
+		res = false;
+		
+		assertEquals("ERREUR : la combinaison vide ne devrait pas être une combination valide ",res,MasterMind.stringChecker(code));
+		
+		//test n°8 - la combinaison ne contient pas de lettre
+		code = ("1234");
+		res = false;
+		
+		assertEquals("ERREUR : 1,2,3,4 ne devrait pas être une combination valide ",res,MasterMind.stringChecker(code));
+		
+		//test n°9 - la combinaison contient des caractères spéciaux
+		code = ("&RRR");
+		res = false;
+		
+		assertEquals("ERREUR : &,R,R,R ne devrait pas être une combination valide ",res,MasterMind.stringChecker(code));
+		
 	}
 
 }
