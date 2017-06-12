3aaa9c2097ff4ce475dd6caffed5f77271870d7f
marmorag <g.marmo@hotmail.fr>
2017-06-12 20:04:33 +0200

    [LOGIC] Added ScenarioParser
    - Added ScenarioParser which convert a file with a defined syntax into a
    playable Scenario
    - Added InvalidSyntaxInFileException
    - Modified syntax in scenario file

--- src/greentower/core/scenario/ScenarioParser.java
+++ src/greentower/core/scenario/ScenarioParser.java
@@ -1,107 +1,277 @@
 package greentower.core.scenario;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import greentower.core.exception.InvalidSyntaxInFileException;
+import greentower.stage.Stage;
+import greentower.stage.choice.Choice;
+import greentower.stage.minigames.lessormore.LessOrMore;
+import greentower.stage.minigames.mastermind.MasterMind;
+import greentower.stage.minigames.othello.core.OthelloGame;
+import greentower.stage.minigames.rushhour.RushHourGame;
+import greentower.stage.minigames.shifumi.Shifumi;
+import greentower.stage.minigames.tictactoe.TicTacToe;
 
 /**
  * This class is a tool to parse Scenario from file and compose Scenario to file
- *
+ * @author Guillaume
  */
 public final class ScenarioParser
 {
 
 	/**
-	 * Default constructor
+	 * The buffered reader used to read the file
 	 */
-	public ScenarioParser()
-	{}
+	BufferedReader inputTool;
+	
+	/**
+	 * The parsed scenario is build on constructor's call
+	 */
+	Scenario parsedScenario;
+	
+	/**
+	 * Default constructor, get a path and build the Scenario from it
+	 * The syntax of the file should be :
+	 * 
+	 * <scenario>
+	 * n => where n is the number of stage
+	 * <dialog>
+	 * dialog in simple text, a \n is symbolized by §
+	 * </dialog>
+	 * <answer>
+	 * answer in simple text each answer of the same stage is separate by §
+	 * </answer>
+	 * <stage>
+	 * the name of the stage, could be : choice mastermind othello tictactoe shifumi lessormore rushhour
+	 * </stage>
+	 * <link>
+	 * n§n where n is the stage number, § the separator
+	 * </link>
+	 * </scenario>
+	 * 
+	 * @param path The path for the Scenario to parse
+	 * @throws FileNotFoundException Is thrown if the given path is incorrect
+	 */
+	public ScenarioParser(String path) throws FileNotFoundException
+	{
+		this.inputTool = new BufferedReader(new FileReader(new File(path)));
+		this.parsedScenario = this.parseScenarioAt(path);
+	}
 	
 	/**
 	 * This method parse a Scenario from the given path
 	 * @param path String the path of the scenario to parse
 	 * @return Scenario the scenario parsed from the file
 	 */
-	public static Scenario parseScenarioAt(String path)
+	private Scenario parseScenarioAt(String path)
 	{
+		String currentLine = "";
+		
+		// variable relative to the scenario to return
+		String[] dialog;
+		String[] answer;
+		int[][] link;
+		Stage[] listOfStage;
 		Scenario scenarioToReturn;
-		ObjectInputStream objectReader;
 		
-		try
+		currentLine = inputTool.readLine();
+		while(currentLine != "<scenario>")
 		{
-			objectReader = new ObjectInputStream(new BufferedInputStream(new FileInputStream(new File(path))));
-
-			try
-			{
-				scenarioToReturn = (Scenario)objectReader.readObject();
-				
-				objectReader.close();
-				return scenarioToReturn;
-			} 
-			catch (ClassNotFoundException e)
-			{
-				System.err.println("Expcetion while reading classfile");
-				e.printStackTrace();
-			} 
-			catch (IOException e)
-			{
-				System.err.println("IOException");
-				e.printStackTrace();
-			}
-		
-		} 
-		catch (IOException e1)
-		{
-			System.err.println("Invalid path");
-			e1.printStackTrace();
+			currentLine = inputTool.readLine();
 		}
 		
-		return null;
+		while(currentLine != "</scenario>")
+		{
+			// recupere le nombre d'etage
+			currentLine = inputTool.readLine();
+			int numberOfStage = Integer.parseInt(currentLine);
+			
+			// cursor should be at <dialog>
+			// recupere les attributs de construction d'un scenario 
+			dialog = this.getDialog(numberOfStage);
+			//cursor should be at <answer>
+			answer = this.getAnswer(numberOfStage);
+			// cursor should be at <stage>
+			listOfStage = this.getListOfStage(numberOfStage, answer, dialog);
+			// cursor should be at <link>
+			link = this.getLink(numberOfStage);
+			// cursor should be at </scenario>
+			
+			// gerer la fin de fichier
+			currentLine = inputTool.readLine();
+		}
+		
+		scenarioToReturn = new Scenario(listOfStage, link, dialog, answer);
+		return scenarioToReturn;
 	}
 	
 	/**
-	 * This method write into a file a Scenario
-	 * @param scnenarioToCompose Scenario the scenario to write in a file
-	 * @return the path of the file
+	 * This method build the list of the stage contained in the file
+	 * @param numberOfStage The number of stage
+	 * @param answer The answer array used to build each Stage
+	 * @param dialog The dialog array used to build each Stage
+	 * @return Stage[] The Stage array builded from the input file
 	 */
-	public static String composeScenarioFrom(Scenario scnenarioToCompose)
+	private Stage[] getListOfStage(int numberOfStage, String[] answer, String[] dialog)
 	{
-		
-		ObjectOutputStream objectWriter;
-		String path = "/bin/scenario.txt";
-		
 		try
 		{
-			objectWriter = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(new File(path))));
+			Stage[] result = new Stage[numberOfStage];
+			String currentLine = this.inputTool.readLine();
+			int stageCounter = 0;
 			
-			try
+			while(currentLine != "</stage>")
 			{
-				objectWriter.writeObject(scnenarioToCompose);
-				objectWriter.close();
-				return path;
-			} catch (IOException e)
-			{
-				System.err.println("Exception while writing object");
-				e.printStackTrace();
+				if(currentLine == "<stage>")
+					currentLine = inputTool.readLine();
+				else
+				{
+					switch (currentLine)
+					{
+						case "choice":
+							result[stageCounter] = new Choice(answer[stageCounter],dialog[stageCounter], stageCounter);
+							break;
+						case "othello":
+							result[stageCounter] = new OthelloGame(dialog[stageCounter], stageCounter);
+							break;
+						case "mastermind":
+							result[stageCounter] = new MasterMind(dialog[stageCounter], stageCounter);
+							break;
+						case "tictactoe":
+							result[stageCounter] = new TicTacToe(dialog[stageCounter], stageCounter);
+							break;
+						case "rushhour":
+							result[stageCounter] = new RushHourGame(dialog[stageCounter], stageCounter);
+							break;
+						case "shifumi":
+							result[stageCounter] = new Shifumi(dialog[stageCounter], stageCounter);
+							break;
+						case "lessormore":
+							result[stageCounter] = new LessOrMore(dialog[stageCounter], stageCounter);
+							break;
+						default:
+							throw new InvalidSyntaxInFileException("Exception during listOfStage build");
+							break;
+							
+					}
+					currentLine = inputTool.readLine();
+					stageCounter++;
+				}
 			}
-		} catch (FileNotFoundException e)
+			return result;
+		} 
+		catch (IOException e)
 		{
-			System.err.println("Invalid path");
-			e.printStackTrace();
-		} catch (IOException e)
-		{
-			System.err.println("Invalid path");
+			System.err.println("Exception during listOfStage build");
 			e.printStackTrace();
 		}
-		return path;
+		return null;
 	}
-	
-	
-	
+
+	/**
+	 * This method build the link matrix which contained the logical link of each stage
+	 * @param numberOfStage The number of stage
+	 * @return	int[][] The builded matrix of logical link
+	 */
+	private int[][] getLink(int numberOfStage)
+	{
+		try
+		{
+			int[][] result = new int[numberOfStage][];
+			String currentLine = this.inputTool.readLine();
+			int stageCounter = 0;
+			
+			while(currentLine != "</link>")
+			{
+				if (currentLine == "<link>")
+					currentLine = inputTool.readLine();
+				else
+				{
+					result[stageCounter][0] = Integer.parseInt(currentLine.split("§")[0]);
+					result[stageCounter][1] = Integer.parseInt(currentLine.split("§")[1]);
+					
+					currentLine = inputTool.readLine();
+					stageCounter++;
+				}				
+			}
+			return result;
+		} 
+		catch (IOException e)
+		{
+			System.err.println("Exception during answer build");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * This method build the answer array from the file
+	 * @param numberOfStage int The number of stage
+	 * @return String[] The string array of answer
+	 */
+	private String[] getAnswer(int numberOfStage)
+	{
+		try
+		{
+			String[] result = new String[numberOfStage];
+			String currentLine = this.inputTool.readLine();
+			int stageCounter = 0;
+			
+			while(currentLine != "</answer>")
+			{
+				if (currentLine == "<answer>")
+					currentLine = inputTool.readLine();
+				else
+				{
+					result[stageCounter] = currentLine.replace('§', '\n');
+					
+					currentLine = inputTool.readLine();
+					stageCounter++;
+				}
+			}
+			return result;
+		} 
+		catch (IOException e)
+		{
+			System.err.println("Exception during answer build");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * This method build the dialog array from the file
+	 * @param numberOfStage int The number of stage
+	 * @return String[] The string array of dialog
+	 */
+	private String[] getDialog(int numberOfStage)
+	{
+		try
+		{
+			String[] result = new String[numberOfStage];
+			String currentLine = this.inputTool.readLine();
+			int stageCounter = 0;
+			
+			while(currentLine != "</dialog>")
+			{
+				if (currentLine == "<dialog>")
+					currentLine = inputTool.readLine();
+				else
+					result[stageCounter] = currentLine.replace('§', '\n');
+				
+				currentLine = inputTool.readLine();
+				stageCounter++;
+			}
+			return result;
+		} 
+		catch (IOException e)
+		{
+			System.err.println("Exception during dialog build");
+			e.printStackTrace();
+		}
+		return null;
+	}	
 }
