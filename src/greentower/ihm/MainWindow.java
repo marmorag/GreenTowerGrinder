94f46309176dcde229f56855e1852b809fbe5be8
marmorag <g.marmo@hotmail.fr>
2017-06-13 15:42:20 +0200

    [LOGIC] Added SoundBox and image for the hangman IHM

--- src/greentower/ihm/MainWindow.java
+++ src/greentower/ihm/MainWindow.java
@@ -1,17 +1,5 @@
 package greentower.ihm;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;import java.awt.event.ActionListener;import java.io.File;import java.io.IOException;import javax.sound.sampled.AudioInputStream;import javax.sound.sampled.AudioSystem;import javax.sound.sampled.Clip;import javax.sound.sampled.LineUnavailableException;import javax.sound.sampled.UnsupportedAudioFileException;import javax.swing.ImageIcon;import javax.swing.JButton;import javax.swing.JFrame;import greentower.core.sound.SoundBox;
 /** *  * @author Rayan * */
 public class MainWindow extends JFrame implements ActionListener{
 	/**
@@ -20,9 +8,7 @@ public class MainWindow extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;	/**	 * The new game button	 */
 	private JButton bt1;		/**	 * The leave button	 */
 	private JButton bt2;	/**	 * The console	 */
-	private Console console; 
-	/**	 * To use sound	 */
-	protected Clip clip;
+	private Console console; 		/**	 * 	 */	protected SoundBox soundBox;
 	/**	 * Set params of JFrame	 */
 	public MainWindow(){
 		this.setTitle("The Green Tower Grinder");
@@ -41,24 +27,12 @@ public class MainWindow extends JFrame implements ActionListener{
 		this.getContentPane().add(bt1);
 		this.getContentPane().add(bt2);
 		this.setVisible(true);
-		//Sound
-		try 
-		{
-			clip = AudioSystem.getClip();
-			AudioInputStream ais = AudioSystem.getAudioInputStream(new File("sound/Skyrim_OST.wav"));
-			clip.open(ais);
-			clip.loop(Clip.LOOP_CONTINUOUSLY);
-		} 
-		catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) 
-		{
-			//e.printStackTrace();
-			System.err.print("SoundException");
-		}
+		//Sound		this.soundBox = new SoundBox();		soundBox.playSoundLoop(SoundBox.SOUND_SKYRIM);		System.out.println("pass");
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if(arg0.getSource() == bt1)
-		{
+		{			this.soundBox.playSoundOnce(SoundBox.SOUND_PLAY);
 			this.dispose();
 			this.console = new Console(this);
 			this.console.setVisible(true);
@@ -68,9 +42,5 @@ public class MainWindow extends JFrame implements ActionListener{
 			this.dispose();
 			new ExitWindow(this);	    
 		}
-	}	/**	 * Stop the game sound	 */
-	public void stopSong()
-	{
-		this.clip.stop();
 	}	/**	 * Getter for the console	 * @return the console	 */	public Console getConsole(){		return this.console;	}
 }
