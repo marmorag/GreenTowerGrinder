40e4641804deb634c4c51f792567ee30c74965b3
marmorag <g.marmo@hotmail.fr>
2017-06-13 10:25:06 +0200

    Revert "Auto stash before merge of "Guillaume" and "origin/Guillaume""
    This reverts commit 67112f3c73dcf5a0aed55e8db6d3e36c752a878a.

--- src/greentower/stage/minigames/rushhour/exceptions/WrongCarException.java
+++ src/greentower/stage/minigames/rushhour/exceptions/WrongCarException.java
@@ -1,36 +1,14 @@
 package greentower.stage.minigames.rushhour.exceptions;
 
+/**
+ * @author Remi
+ *
+ */
 public class WrongCarException extends Exception
 {
-
 	/**
-	 * 
+	 * Serial Version id
 	 */
 	private static final long serialVersionUID = 1L;
 
 }
-
-public class WrongCarException extends Exception
-{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-}
-
-package greentower.stage.minigames.rushhour.exceptions;
-
-/**
- * @author Remi
- *
- */
-public class WrongCarException extends Exception
-{
-	/**
-	 * Serial Version id
-	 */
-	private static final long serialVersionUID = 1L;
-
-}
