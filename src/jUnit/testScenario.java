336c5f8f864c6de750e8bb0a908fe486a56d6081
Axelle <Axelle@DESKTOP-GQP648J.home>
2017-06-13 15:11:44 +0200

    [TEST] testScenario finished

--- src/jUnit/testScenario.java
+++ src/jUnit/testScenario.java
@@ -27,55 +27,56 @@ public class testScenario {
 		int lastResult;
 		int res;
 
-		//test n°1 : test de l'étage 15
-		currentStage = 15;
-
+		
+		//test n°2
+		currentStage = 2;
 		//cas 1
 		lastResult = 0;
-		res = 17;
-
-		//assertEquals("L'étage 15 abouti au 17 si la réponse est 0",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
-
+		res = 3;
+		assertEquals("L'étage 2 abouti au 3 si la réponse est 0",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
+		assertEquals("",scenario.getNextStages(currentStage, lastResult).getIndex(),res);
 		//cas 2
 		lastResult = 1;
-		res = 18;
-
-		//assertEquals("L'étage 15 abouti au 18 si la réponse est 1",scenario.getNextStages(currentStage, lastResult).getIndex(),res); //$NON-NLS-1$
-
-
-		//test n°2
+		res = 4;
+		assertEquals("L'étage 2 abouti au 4 si la réponse est 1",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
+		assertEquals("",scenario.getNextStages(currentStage, lastResult).getIndex(),res);
+		
+		
+		//test n°3
+		currentStage = 3;
+		//cas 1 
+		lastResult = 0;
+		res = 5;
+		assertEquals("L'étage 3 abouti au 5 si la réponse est 1",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
+		assertEquals("",scenario.getNextStages(currentStage, lastResult).getIndex(),res);
+		//cas 2
+		lastResult = 1;
+		res = 6;
+		assertEquals("L'étage 3 abouti au 6 si la réponse est 1",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
+		assertEquals("",scenario.getStageIndex(scenario.getNextStages(currentStage, lastResult)),res);
+		
+		
+		//test n°9
 		currentStage = 9;
-
 		//cas 1
 		lastResult = 0;
 		res = 12;
-
 		assertEquals("L'étage 9 abouti au 12 si la réponse est 0",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
-
-		//cas 2
-		lastResult = 1;
-		res = 13;
-
-		//assertEquals("L'étage 9 abouti au 13 si la réponse est 1",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
-
-
-		//test n°3
-		currentStage = 2;
-
+		assertEquals("",scenario.getNextStages(currentStage, lastResult).getIndex(),res);
+		
+		
+		//test n°15 : test de l'étage 15
+		currentStage = 15;
 		//cas 1
 		lastResult = 0;
-		res = 0;
-
-		//assertEquals("L'étage 2 abouti au 3 si la réponse est 0",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
-
-
+		res = 20;
+		assertEquals("L'étage 15 abouti au 17 si la réponse est 0",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
+		assertEquals("",scenario.getNextStages(currentStage, lastResult).getIndex(),res);
 		//cas 2
 		lastResult = 1;
-		res = 2;
-
-		//assertEquals("L'étage 2 abouti au 2 si la réponse est 1",scenario.getNextStages(currentStage, lastResult),scenario.getStageAt(res)); //$NON-NLS-1$
-
-
+		res = 21;
+		assertEquals("L'étage 15 abouti au 18 si la réponse est 1",scenario.getNextStages(currentStage, lastResult).getIndex(),res); //$NON-NLS-1$
+		assertEquals("",scenario.getNextStages(currentStage, lastResult).getIndex(),res);	
 
 	}
 
